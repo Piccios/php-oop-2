8750e81797ae73c7af7738d820a166501805b699
Piccios <lorenzo.hl.picchi@gmail.com>
2024-07-03 15:59:23 +0200

    correciton in species class set public functions to get and set species type

--- src/classes/Species.php
+++ src/classes/Species.php
@@ -1,8 +1,23 @@
 <?php
 
-class Species {
-    const dogs = 'Dogs';
-    const cats = 'Cats';
-    const birds = 'Birds';
-    const fish = 'Fish';
+class Species
+{
+    private $species;
+
+    public function __construct($species){
+        $this->species = $species;
+    }
+
+    /**
+     * This function returns the species 
+     *
+     * @return void
+     */
+    public function getSpecies(){
+        return $this->species;
+    }
+
+    public function setSpecies($species) {
+        $this->species = $species;
+    }
 }
