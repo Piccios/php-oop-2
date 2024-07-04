625d493370d82497c55459baab3c0f016a183d29
Piccios <lorenzo.hl.picchi@gmail.com>
2024-07-04 10:59:04 +0200

    products are now well displayed in page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/classes/Accessorise.php';
 require_once __DIR__ . '/classes/Food.php';
 require_once __DIR__ . '/classes/Species.php';
 require_once __DIR__ . '/classes/Toys.php';
-require_once __DIR__ . '/classes/products.php';
+require_once __DIR__ . '/classes/Products.php';
 
 $dog = new Species('Dog');
 $cat = new Species('Cat');
@@ -11,17 +11,16 @@ $bird = new Species('Bird');
 $fish = new Species('Fish');
 
 $products = [
-    new Food('Royal Canin Mini Adult', $dog, 29.80, "Chiken",'https://arcaplanet.vtexassets.com/arquivos/ids/300306/Royal-Canin-Mini-Adult-Alimento-Completo-per-Cani-Adulti-di-Piccola-Taglia-10120604-1.jpg'), 
+    new Food('Royal Canin Mini Adult', $dog, 29.80, "Chiken", 'https://arcaplanet.vtexassets.com/arquivos/ids/300306/Royal-Canin-Mini-Adult-Alimento-Completo-per-Cani-Adulti-di-Piccola-Taglia-10120604-1.jpg'),
     new Food('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', $dog, 38.90,  "Chiken", 'https://arcaplanet.vtexassets.com/arquivos/ids/300514/Almo-Nature-Holistic-Maintenance-Medium-Large-Tonno-e-Riso-10118806.jpg'),
-    new Food('Almo Nature Cat Daily Lattina', $cat,34.99, "Chiken", 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg'),
-    new Food('Mangime per Pesci Guppy in Fiocchi', $fish,3.95,"Chiken", 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg'),
+    new Food('Almo Nature Cat Daily Lattina', $cat, 34.99, "Chiken", 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg'),
+    new Food('Mangime per Pesci Guppy in Fiocchi', $fish, 3.95, "Chiken", 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg'),
     new Accessorise('Voliera Wilma in Legno', 190, $bird, 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg'),
     new Accessorise('Cartucce Filtranti per Filtro EasyCrystal', 1.80, $fish, 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg'),
     new Toys('Kong Classic', 15, $dog, 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg'),
     new Toys('Topini di peluche Trixie', 9, $cat, 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg'),
 ];
 
-var_dump($products);
 ?>
 
 
@@ -38,26 +37,29 @@ var_dump($products);
 </head>
 
 <body>
-    <header>
+    <header class="d-flex justify-content-center bg-dark text-white">
         <h1>OOP Shop</h1>
     </header>
     <main>
         <section class="container">
             <div class="row">
-                <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">Product</h5>
-                            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse hic minus aliquam accusamus, iure ratione officiis eos maiores quod eligendi natus ullam tenetur molestias fugit in consequuntur iste, error excepturi?</p>
-                            <a href="#" class="btn btn-primary">Add to cart</a>
+                <?php
+                foreach ($products as $product) { ?>
+                    <div class="col-3">
+                        <div class="card m-5" style="width: 18rem;">
+                            <img src="<?php echo $product->getImgUrl() ?>" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $product->getName() ?></h5>
+                                <p class="card-text"> Price: <?php echo $product->getPrice() ?></p>
+                                <a href="#" class="btn btn-primary">Add to cart</a>
+                            </div>
                         </div>
+                    <?php } ?>
                     </div>
-                </div>
             </div>
         </section>
     </main>
-    <footer>
+    <footer class="d-flex justify-content-center bg-dark text-white">
         <p>Copyright &copy; 2024</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
